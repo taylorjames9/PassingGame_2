22b4dcb45e2abebd7c460a6a84676e77a1f453d3
James Taylor <taylorjames9@gmail.com>
2014-02-21 17:55:45 -0600

    Switch the main project file

---
+++ PassingGame_Inner_2/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+
+	static int speed = 5;
+	//static int dampening = 10;
+
+	void  Update () {
+
+
+		if (Input.GetKey (KeyCode.UpArrow)) rigidbody.AddForce(Vector3.up * 3);
+		if (Input.GetKey (KeyCode.DownArrow)) rigidbody.AddForce(Vector3.down * 3);
+		if (Input.GetKey (KeyCode.LeftArrow)) rigidbody.AddForce(Vector3.left * 3);
+		if (Input.GetKey (KeyCode.RightArrow)) rigidbody.AddForce(Vector3.right * 3);
+
+
+		if (Input.GetKey (KeyCode.Space))
+			ShowLayers ();
+	}
+
+	void ShowLayers(){
+		print ("show space running");
+	}
+}
