77b502fb3aa0042b55e1af929f9cac0c85eddc52
James Taylor <taylorjames9@gmail.com>
2014-02-21 19:12:33 -0600

    NICE FOLLOW WORKING

---
+++ PassingGame_Inner_2/Assets/Scripts/CharacterController_1.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController_1 : MonoBehaviour {
+
+
+	//static int speed = 5;
+	//static int dampening = 10;
+	public GameObject selfLayer1;
+	public GameObject selfLayer2;
+	public GameObject selfLayer3;
+
+
+
+
+	void  Update () {
+
+
+		if (Input.GetKey (KeyCode.UpArrow))
+			rigidbody.AddForce (Vector3.up * 10);
+		if (Input.GetKey (KeyCode.DownArrow))
+			rigidbody.AddForce (Vector3.down * 10);
+		if (Input.GetKey (KeyCode.LeftArrow))
+			rigidbody.AddForce (Vector3.left * 10);
+		if (Input.GetKey (KeyCode.RightArrow))
+			rigidbody.AddForce (Vector3.right * 10);
+
+
+		if (Input.GetKey (KeyCode.Space)) {
+			ShowLayers ();
+		}
+	}
+		
+
+
+
+	void ShowLayers(){
+		print ("show space running");
+
+		//selfLayer1.transform.position.x = transform.position.x-1;
+		//selfLayer2.transform.position.x = transform.position.x-2;
+
+	}
+}
---
+++ PassingGame_Inner_2/Assets/Scripts/UnderLayerScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnderLayerScript : MonoBehaviour {
+
+		public Transform target;
+		public float smoothTime;
+	public float zOffSet;
+		private Vector3 velocity = Vector3.zero;
+		void Update() {
+		Vector3 targetPosition = new Vector3 (target.transform.position.x, target.transform.position.y, zOffSet);
+
+
+			transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+		}
+}
