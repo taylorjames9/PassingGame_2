9ed13ca28779220fed2ae6da6b80f3da6ab6a9b7
James Taylor <taylorjames9@gmail.com>
2014-02-22 21:25:36 -0600

    A glitchy working of position shuffle works

--- PassingGame_Inner_2/Assets/Scripts/CharacterController_1.cs
+++ PassingGame_Inner_2/Assets/Scripts/CharacterController_1.cs
@@ -10,6 +10,10 @@ public class CharacterController_1 : MonoBehaviour {
 	public GameObject selfLayer2;
 	public GameObject selfLayer3;
 
+	UnderLayerScript selfLayerScript1;
+	UnderLayerScript selfLayerScript2;
+	UnderLayerScript selfLayerScript3;
+
 	public float layerOrder;
 
 	public float forceSplay;
@@ -20,6 +24,14 @@ public class CharacterController_1 : MonoBehaviour {
 	void Start(){
 		print (selfLayer1.transform.localPosition);
 		forceSplay = 0.000005f;
+
+		selfLayerScript1 = selfLayer1.GetComponent<UnderLayerScript>();
+		selfLayerScript2 = selfLayer2.GetComponent<UnderLayerScript>();
+		selfLayerScript3 = selfLayer3.GetComponent<UnderLayerScript>();
+
+
+
+
 	}
 
 	void  FixedUpdate () {
@@ -36,7 +48,7 @@ public class CharacterController_1 : MonoBehaviour {
 			rigidbody.AddForce (Vector3.right * 5);
 
 
-		if (Input.GetKey (KeyCode.Space)) {
+		if (Input.GetKeyUp (KeyCode.Space)) {
 			SplayShuffle ();
 		} 
 	}
@@ -46,6 +58,14 @@ public class CharacterController_1 : MonoBehaviour {
 		selfLayer1.rigidbody.velocity = new Vector3 (0, 10, 0);
 		selfLayer2.rigidbody.velocity = new Vector3 (10, 0, 0);
 		selfLayer3.rigidbody.velocity = new Vector3 (-10, 0, 0);
+
+		/*selfLayer1.transform.position = new Vector3 (selfLayer1.transform.position.x, selfLayer1.transform.position.y,selfLayer2.transform.position.z);
+		selfLayer2.transform.position = new Vector3 (selfLayer2.transform.position.x, selfLayer2.transform.position.y,selfLayer3.transform.position.z);
+		selfLayer3.transform.position = new Vector3 (selfLayer3.transform.position.x, selfLayer3.transform.position.y,selfLayer1.transform.position.z);*/
+
+		selfLayerScript1.zOffSet = selfLayerScript2.zOffSet;
+		selfLayerScript2.zOffSet = selfLayerScript3.zOffSet;
+		selfLayerScript3.zOffSet = selfLayerScript1.zOffSet;
 
 		StartCoroutine (StopVelocity ());
 
